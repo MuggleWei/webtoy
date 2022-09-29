4239e98d623b80341c7cf93090073154a7ec407a
Muggle Wei <mugglewei@gmail.com>
2022-09-29 23:25:03 +0800

    update Profile page

--- frontend/webtoy-front/src/pages/Profile.js
+++ frontend/webtoy-front/src/pages/Profile.js
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
-import React, { useEffect } from "react";
+import { Box, Container, Paper } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import requests from "../utils/requests";
 import url from "../utils/url";
@@ -7,6 +7,8 @@ import url from "../utils/url";
 function Profile() {
     let navigate = useNavigate();
     let location = useLocation();
+
+    const [userProfileInfo, setUserProfileInfo] = useState(null);
 
     useEffect(() => {
         requests
@@ -18,15 +20,26 @@ function Profile() {
                         state: { from: location }
                     });
                 } else {
-                    // TODO: load profile informations
+                    console.info(rsp);
+                    setUserProfileInfo(rsp.data);
                 }
             })
-    })
+    }, [])
 
-    return (
-        <Box>
-            Profile Page
-        </Box>
+    return (userProfileInfo
+        ?
+        <Container>
+            <div>
+                {userProfileInfo.show_name}
+            </div>
+            <div>
+                <h3>User ID: {userProfileInfo.uid}</h3>
+                <h3>Email: {userProfileInfo.email}</h3>
+                <h3>Phone: {userProfileInfo.phone}</h3>
+            </div>
+        </Container>
+        :
+        <></>
     );
 }
 
